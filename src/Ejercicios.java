2dd616fc19e9cb91a258cfec0497ceae91695756
FranciscoETSD <tadeo00704@gmail.com>
2021-11-19 23:40:28 -0400

    Ejercicio FIBONACCI

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicios {
     /*
+    --COMPARAR DOS VECTORES CON RECURSIVIDAD--
     Metodo recursivo para comparar dos vectores de enteros elemento por elemento
     vector1 = {5, 6, 2}
     vector2 = {2, 9, 2}
     diferen = 3 + 3 + 0 = 6
      */
-    private  static int compararVectores(int[] vector1, int[] vector2, int indice) {
+    private static int compararVectores(int[] vector1, int[] vector2, int indice) {
         int sumaDiferencias;
 
-        if(indice == vector1.length - 1){ //caso base
+        if (indice == vector1.length - 1) { //caso base
             sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]);
         } else { //llamada recursiva
             sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]) + compararVectores(vector1, vector2, indice + 1);
@@ -16,10 +19,39 @@ public class Ejercicios {
 
         return sumaDiferencias;
     }
+
     /*
     Metodo auxiliar que realiza la llamada al metodo recursivo
      */
     public static boolean compararVectores(int[] vector1, int[] vector2) {
         return compararVectores(vector1, vector2, 0) == 0;
     }
+
+/*
+--FIBONACI--
+--Diseñar e implementar un método recursivo para calcular el enésimo término de
+ la sucesión--
+ */
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduce el número de elementos a mostrar de la serie: ");
+    int limite = sc.nextInt();
+    sc.close();
+
+    for(int i = 0; i<limite; i++){
+        System.out.print(funcionFibonacci(i) + ", ");
+    }
 }
+    private static int funcionFibonacci(int num){
+        if(num == 0 || num==1)
+            return num;
+        else
+            return funcionFibonacci(num-1) + funcionFibonacci(num-2);
+    }
+}
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int[] vector1 = {5, 6, 2};
-        int[] vector2 = {5, 6, 8};
-        System.out.println(Ejercicios.compararVectores(vector1, vector2));
+         //--COMPARAR DOS VECTORES CON RECURSIVIDAD--
+        //int[] vector1 = {5, 6, 2};
+        //int[] vector2 = {5, 6, 8};
+        //System.out.println(Ejercicios.compararVectores(vector1, vector2));
+
+        /*--FIBONACI--*/
+        //Ejecutar en la parte de ejercicios//
     }
 }
